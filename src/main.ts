def28c0f0e92626e238d331845bc450458a840e3
gokul3710 <gokulkrishnawaryar@gmail.com>
2023-11-07 02:20:45 -0800

    allow origin all

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { config } from 'dotenv';
-import { domains } from './core/constants/domains';
 config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions = {
-    origin: [
-      domains.DOMAIN,
-      domains.DOMAIN_2,
-      domains.DOMAIN_LOCALHOST,
-      domains.ADMIN_DOMAIN,
-      domains.ADMIN_DOMAIN_2,
-      domains.ADMIN_DOMAIN_LOCALHOST,
-    ],
+    origin: '*',
     methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH'],
     credentials: true,
   };
